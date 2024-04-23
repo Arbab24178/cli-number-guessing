295afc3ce0690284f05c2d1900c736929c149a56
Arbab Khan <148271563+Arbab24178@users.noreply.github.com>
2024-04-24 03:18:26 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,26 @@
+#! /urs/bin/env node
+
+import inquirer from "inquirer";
+
+//   1) Computer will generate a random number
+
+//   2) User input for guessing number
+
+//   3) Compare user input with computer generated number and show result
+
+
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please guess a number between 1 to 6 : ",
+    },
+])
+
+if(answer.userGuessedNumber === randomNumber){
+    console.log("Congtratulations! guessed the right number.");
+}
+else{
+    console.log(" You guessed the wrong number.");
+}
